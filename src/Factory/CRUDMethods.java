a9449809a7be2fddcbb33d4aa90014d652d0628a
jefcarmo0103 <jefcarmo01@hotmail.com>
2019-05-21 15:58:00 -0300

    modificações no CRUD Principal

--- src/Factory/CRUDMethods.java
+++ src/Factory/CRUDMethods.java
@@ -2,13 +2,9 @@ package Factory;
 
 import Interfaces.ICRUD;
 
-import java.sql.Connection;
-import java.sql.PreparedStatement;
-import java.sql.SQLException;
+import java.sql.*;
 import java.sql.Date;
-import java.util.ArrayList;
-import java.util.Calendar;
-import java.util.Dictionary;
+import java.util.*;
 
 public class CRUDMethods implements ICRUD {
 
@@ -21,30 +17,87 @@ public class CRUDMethods implements ICRUD {
 
 
     @Override
-    public ArrayList<?> getAll(String sqlQuery) {
-        //smt = connection.prepareStatement(sqlQuery);
-        return null;
+    public Map<?,?> getAll(String sqlQuery) {
 
+        ResultSet rs = ExecutaSQL(null,sqlQuery);
+        Map<String,String> result = makeMap(rs);
+
+        return result;
     }
 
     @Override
-    public ArrayList<?> getById(Object objSelId, String sqlQuery) {
-        return null;
+    public Map<?,?> getById(Object objSelId, String sqlQuery) {
+
+        Dictionary<Integer, Object> parameters = (Dictionary<Integer, Object>)objSelId;
+
+        ResultSet rs = ExecutaSQL(parameters,sqlQuery);
+        Map<String,String> result = makeMap(rs);
+
+        return result;
+
     }
 
     @Override
     public void Add(Object objAdd, String sqlQuery) {
+        Dictionary<Integer, Object> parameters = (Dictionary<Integer, Object>)objAdd;
+        ExecuteQuery(parameters, sqlQuery);
 
     }
 
     @Override
     public void Delete(Object objDel, String sqlQuery) {
-
+        Dictionary<Integer, Object> parameters = (Dictionary<Integer, Object>)objDel;
+        ExecuteQuery(parameters, sqlQuery);
     }
 
     @Override
-    public void Update(Object objAdd, String sqlQuery) {
+    public void Update(Object objUpd, String sqlQuery) {
+        Dictionary<Integer, Object> parameters = (Dictionary<Integer, Object>)objUpd;
+        ExecuteQuery(parameters, sqlQuery);
+    }
 
+    private Map<String, String> makeMap(ResultSet rs){
+
+        Map<String,String> result = null;
+        try{
+            List<String> columnNames = new ArrayList<>();
+            ResultSetMetaData rsmd = rs.getMetaData();
+            for (int i = 1; i <= rsmd.getColumnCount(); i++) {
+                columnNames.add(rsmd.getColumnLabel(i));
+            }
+
+            int rowIndex = 0;
+            while (rs.next()) {
+                rowIndex++;
+
+                List<Object> rowData = new ArrayList<>();
+                for (int i = 1; i <= rsmd.getColumnCount(); i++) {
+                    rowData.add(rs.getObject(i));
+                }
+
+                System.out.printf("Row %d%n", rowIndex);
+                for (int colIndex = 0; colIndex < rsmd.getColumnCount(); colIndex++) {
+                    String objType = "null";
+                    String objString = "";
+                    Object columnObject = rowData.get(colIndex);
+                    if (columnObject != null) {
+                        objString = columnObject.toString() + " ";
+                        objType = columnObject.getClass().getName();
+                    }
+                    System.out.printf("  %s: %s(%s)%n",
+                            columnNames.get(colIndex), objString, objType);
+
+                    result.put(columnNames.get(colIndex), objString);
+                }
+            }
+        }
+        catch(SQLException e){
+            throw new RuntimeException(e);
+        }
+
+
+
+        return result;
     }
 
     private void ExecuteQuery(Dictionary<Integer, Object> params, String sql){
@@ -57,6 +110,27 @@ public class CRUDMethods implements ICRUD {
         catch(SQLException e){
             throw new RuntimeException(e);
         }
+    }
+
+    private ResultSet ExecutaSQL (Dictionary<Integer, Object> params, String sql){
+        ResultSet rs = null;
+
+        try{
+            smt = connection.prepareStatement(sql);
+
+            if(params != null)
+                smt = setParameters(params);
+
+            rs = smt.executeQuery();
+            rs.close();
+            smt.close();
+
+        }
+        catch(SQLException e){
+            throw new RuntimeException(e);
+        }
+
+        return rs;
     }
 
 
--- src/Interfaces/ICRUD.java
+++ src/Interfaces/ICRUD.java
@@ -2,10 +2,11 @@ package Interfaces;
 
 import java.sql.PreparedStatement;
 import java.util.ArrayList;
+import java.util.Map;
 
 public interface ICRUD <T, U extends String>{
-	ArrayList<?> getAll(U sqlQuery);
-	ArrayList<?> getById(T objSelId, U sqlQuery);
+	Map<?,?> getAll(U sqlQuery);
+	Map<?,?> getById(T objSelId, U sqlQuery);
 	void Add(T objAdd, U sqlQuery);
 	void Delete(T objDel, U sqlQuery);
 	void Update(T objAdd, U sqlQuery);
